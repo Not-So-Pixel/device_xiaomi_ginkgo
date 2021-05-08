9857835d3d19493ad08ccd63c827273a4baff4b9
whyredfire <whyredfire@gmail.com>
2021-05-08 23:57:12 +0530

    ginkgo: drop pixel charger flag - not needed for PixelOS

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -25,7 +25,6 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common PixelOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_FACE_UNLOCK := true
 TARGET_GAPPS_ARCH := arm64
